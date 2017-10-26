fb2e12e07da0841df03c6b91a058efcd03a27bfa
christos christodoulou <xxristos@gmail.com>
2017-10-26 15:45:39 +0300

    initial commit

---
+++ FAPeripheralAccelerometerSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FAPeripheralAccelerometerSample
+//
+//  Created by Christos Christodoulou on 26/10/2017.
+//  Copyright © 2017 Christos Christodoulou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FAPeripheralAccelerometerSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FAPeripheralAccelerometerSample
+//
+//  Created by Christos Christodoulou on 26/10/2017.
+//  Copyright © 2017 Christos Christodoulou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
