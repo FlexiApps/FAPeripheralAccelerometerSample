675844c146f0d784cdde7d968de1880649fd383f
christos christodoulou <xxristos@gmail.com>
2017-10-26 15:45:39 +0300

    initial commit

---
+++ FAPeripheralAccelerometerSample/FAPeripheralAccelerometerSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FAPeripheralAccelerometerSample
+//
+//  Created by Christos Christodoulou on 26/10/2017.
+//  Copyright © 2017 Christos Christodoulou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
